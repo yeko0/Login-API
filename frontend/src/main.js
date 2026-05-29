f87e1d041d9233a1ced01550ec89f0722c3070bd
yeko0 <yekogermany@gmail.com>
2026-05-29 19:29:07 +0200

    Add backend status indicator logic with JS

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,1 +1,16 @@
 import './style.css'
+
+const statusDot = document.getElementById("backend-status-dot");
+const statusText = document.getElementById("backend-status-text");
+
+const backendIsOnline = false;
+
+setTimeout(() => {
+    if(backendIsOnline){
+        statusDot.className = "text-green-400";
+        statusText.textContent = "localhost:8081 online";
+    }else{
+        statusDot.className = "text-red-400";
+        statusText.textContent = "localhost:8081 offline";
+    }
+}, 3000);
