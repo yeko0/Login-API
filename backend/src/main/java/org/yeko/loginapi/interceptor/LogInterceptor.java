748237acc28817cc8de2ce72b0962e89fcbb8680
yeko0 <yekogermany@gmail.com>
2026-07-08 13:41:32 +0200

    refactor: replace System.out with slf4j logging in LogInterceptor

--- backend/src/main/java/org/yeko/loginapi/interceptor/LogInterceptor.java
+++ backend/src/main/java/org/yeko/loginapi/interceptor/LogInterceptor.java
@@ -3,17 +3,21 @@ package org.yeko.loginapi.interceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jspecify.annotations.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class LogInterceptor implements HandlerInterceptor {
+    private static final Logger log = LoggerFactory.getLogger(LogInterceptor.class);
 
     @Override
     public boolean preHandle(@NonNull HttpServletRequest request,
                              @NonNull HttpServletResponse response,
                              @NonNull Object handler) throws Exception {
-        System.out.println("Request: "+ request.getMethod() +" "+ request.getRequestURI());
+
+        log.info("Request: {} {}", request.getMethod(), request.getRequestURI());
         return true;
     }
 }
