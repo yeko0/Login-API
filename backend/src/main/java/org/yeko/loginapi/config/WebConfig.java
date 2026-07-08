e1ac7c74c1b2a0027396ddd8ff57aa1b66bb727a
yeko0 <yekogermany@gmail.com>
2026-07-08 11:26:33 +0200

    remove: legacy old code in frontend and change port number im crossOrigin

--- backend/src/main/java/org/yeko/loginapi/config/WebConfig.java
+++ backend/src/main/java/org/yeko/loginapi/config/WebConfig.java
@@ -25,8 +25,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3307")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
